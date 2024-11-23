5e9cd7c15ba6e8706446dce03c5205c07e49cb41
Mizu <enzomarquez2005@gmail.com>
2024-11-23 01:04:37 -0500

    started pickaxe and potion

---
+++ Pickaxe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monster_Hunter
+{
+    public class Pickaxe
+    {
+
+        //check if the pickaxe breaks after use
+        public bool BreakPickaxe()
+        {
+           
+            int chance = SingletonRandom.GetInstance().GetRandomNumber(1, 4); // Random value between 1 and 3 for pickaxe break chance
+
+            // If the random number is 1, the pickaxe breaks
+            if (chance == 1)
+            {
+                Console.WriteLine("The pickaxe broke!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("The pickaxe did not break.");
+                return false;
+            }
+        }
+    }
+}
---
+++ Potion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monster_Hunter
+{
+    public enum PotionType
+    {
+        Poisoned,
+        Speed,
+        Invisibility,
+        Strength
+    }
+
+    public class Potion
+    {
+        // Property to hold the type of potion
+        public PotionType Type { get; private set; }
+
+        // Constructor
+        public Potion()
+        {
+            // Roll a 6-sided dice to determine potion type
+            int diceRoll = SingletonRandom.GetInstance().GetRandomNumber(1, 7); // Random number between 1 and 6
+
+            switch (diceRoll)
+            {
+                case 1:
+                    Type = PotionType.Poisoned;
+                    break;
+                case 2:
+                    Type = PotionType.Speed;
+                    break;
+                case 3:
+                    Type = PotionType.Speed;
+                    break;
+                case 4:
+                    Type = PotionType.Invisibility;
+                    break;
+                case 5:
+                    Type = PotionType.Invisibility;
+                    break;
+                case 6:
+                    Type = PotionType.Strength;
+                    break;
+            }
+
+            Console.WriteLine($"Potion created: {Type}");
+        }
+
+
+    }
+        
+}
